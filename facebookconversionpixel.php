0220bd3b9c05f9b10e948ae8b426843f26fc676b
Jon goldberg <jon@palantetech.coop>
2015-04-20 14:48:53 -0400

    Initial commit

---
+++ facebookconversionpixel.php
@@ -0,0 +1,120 @@
+<?php
+
+require_once 'facebookconversionpixel.civix.php';
+
+/**
+ * Implementation of hook_civicrm_buildForm
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_buildForm
+ */
+function facebookconversionpixel_civicrm_buildForm($formName, &$form) {
+  if($formName == 'CRM_Contribute_Form_Contribution_ThankYou') {
+    CRM_Core_Resources::singleton()->addScriptFile('coop.palantetech.facebookconversionpixel', 'facebook_conversion_pixel.js');
+  }
+}
+
+
+/**
+ * Implementation of hook_civicrm_config
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
+ */
+function facebookconversionpixel_civicrm_config(&$config) {
+  _facebookconversionpixel_civix_civicrm_config($config);
+}
+
+/**
+ * Implementation of hook_civicrm_xmlMenu
+ *
+ * @param $files array(string)
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
+ */
+function facebookconversionpixel_civicrm_xmlMenu(&$files) {
+  _facebookconversionpixel_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implementation of hook_civicrm_install
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
+ */
+function facebookconversionpixel_civicrm_install() {
+  _facebookconversionpixel_civix_civicrm_install();
+}
+
+/**
+ * Implementation of hook_civicrm_uninstall
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
+ */
+function facebookconversionpixel_civicrm_uninstall() {
+  _facebookconversionpixel_civix_civicrm_uninstall();
+}
+
+/**
+ * Implementation of hook_civicrm_enable
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
+ */
+function facebookconversionpixel_civicrm_enable() {
+  _facebookconversionpixel_civix_civicrm_enable();
+}
+
+/**
+ * Implementation of hook_civicrm_disable
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
+ */
+function facebookconversionpixel_civicrm_disable() {
+  _facebookconversionpixel_civix_civicrm_disable();
+}
+
+/**
+ * Implementation of hook_civicrm_upgrade
+ *
+ * @param $op string, the type of operation being performed; 'check' or 'enqueue'
+ * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
+ *
+ * @return mixed  based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
+ *                for 'enqueue', returns void
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
+ */
+function facebookconversionpixel_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _facebookconversionpixel_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implementation of hook_civicrm_managed
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
+ */
+function facebookconversionpixel_civicrm_managed(&$entities) {
+  _facebookconversionpixel_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implementation of hook_civicrm_caseTypes
+ *
+ * Generate a list of case-types
+ *
+ * Note: This hook only runs in CiviCRM 4.4+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ */
+function facebookconversionpixel_civicrm_caseTypes(&$caseTypes) {
+  _facebookconversionpixel_civix_civicrm_caseTypes($caseTypes);
+}
+
+/**
+ * Implementation of hook_civicrm_alterSettingsFolders
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
+ */
+function facebookconversionpixel_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
+  _facebookconversionpixel_civix_civicrm_alterSettingsFolders($metaDataFolders);
+}
